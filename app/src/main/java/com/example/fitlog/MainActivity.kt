ede86533a80477374393955844b5bff6b6ead498
Ebrar Tikit <20200808066@ogr.akdeniz.edu.tr>
2025-04-01 22:04:32 +0300

    feat: Implement splash screen and update color scheme

--- app/src/main/java/com/example/fitlog/MainActivity.kt
+++ app/src/main/java/com/example/fitlog/MainActivity.kt
@@ -4,13 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.example.fitlog.ui.screens.splash.SplashScreen
 import com.example.fitlog.ui.theme.FitLogTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +23,25 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             FitLogTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                MainScreen()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FitLogTheme {
-        Greeting("Android")
+fun MainScreen() {
+    var showSplashScreen by remember { mutableStateOf(true) }
+    
+    if (showSplashScreen) {
+        SplashScreen(onTimeout = { showSplashScreen = false })
+    } else {
+        // Your main app content goes here
+        Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+            Text(text = "Welcome to FitLog")
+        }
     }
 }
+
+
+
