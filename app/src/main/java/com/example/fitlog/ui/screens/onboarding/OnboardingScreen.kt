34c4e2e7d341ba9063cdf22b65826a9f082d4872
Ebrar Tikit <20200808066@ogr.akdeniz.edu.tr>
2025-04-01 23:18:37 +0300

    feat: Create onboarding screen composable

---
+++ app/src/main/java/com/example/fitlog/ui/screens/onboarding/OnboardingScreen.kt
@@ -0,0 +1,74 @@
+package com.example.fitlog.ui.screens.onboarding
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.room.util.TableInfo
+import com.example.fitlog.data.model.Onboarding
+
+
+@Composable
+fun OnboardingScreen(onboarding: Onboarding) {
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .background(color = Color.White)) {
+        Box(modifier = Modifier) {
+            Image(
+                painter = painterResource(id = onboarding.image),
+                contentDescription = null,
+                modifier = Modifier.fillMaxWidth(),
+                contentScale = ContentScale.FillWidth
+            )
+            Text(
+                text = "Skip",
+                color = Color.Gray,
+                fontSize = 16.sp,
+                modifier = Modifier
+                    .align(Alignment.TopEnd)
+                    .padding(16.dp)
+            )
+        }
+        Text(
+            text = onboarding.title,
+            modifier = Modifier
+                .fillMaxWidth(),
+            fontSize = 24.sp,
+            textAlign = TextAlign.Center,
+            style = MaterialTheme.typography.titleMedium,
+            color = MaterialTheme.colorScheme.onBackground,
+        )
+        Text(
+            text = onboarding.description,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            fontSize = 16.sp,
+            textAlign = TextAlign.Center,
+            style = MaterialTheme.typography.bodyMedium,
+            color = MaterialTheme.colorScheme.onBackground,
+        )
+    }
+}
+
+@Preview
+@Composable
+fun PreviewOnboardingScreen() {
+    OnboardingScreen(Onboarding.First)
+}
